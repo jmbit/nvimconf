dc657dd60fb2de4e4da92548b5d57cb99603c463
Johannes Bülow <johannes@jmbit.de>
2024-05-13 08:52:39 +0200

    minimal working config for most languages

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,12 +5,14 @@
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+	theme = "radium",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true,
+    fg = "blue",
+    standout = true},
+		["@comment"] = { italic = true },
+	},
 }
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,31 +8,62 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+        "gopls",
+        "rust-analyzer",
+        "black",
+        "debugpy",
+        "mypy",
+        "ruff-lsp",
+        "pyright",
+        "templ",
+        "clangd",
+        "cpplint",
+        "cpptools",
+        "cmake-language-server",
+        "bash-language-server",
+        "dockerfile-language-server",
+        "gh",
+        "go-debug-adapter",
+        "html-lsp",
+        "htmx-lsp",
+        "htmlbeautifier",
+        "helm-ls",
+        "htmlhint",
+        "jq",
+        "jq-lsp",
+        "json-lsp",
+        "lua-language-server",
+        "prettier",
+        "pyright",
+        "salt-lsp",
+        "shellcheck",
+        "shellharden",
+        "sqls",
+        "sqlfluff",
+        "yaml-language-server",
+        "ansible-language-server",
+        "ansible-lint",
+  		},
+  	},
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "go", "templ", "rust", "sql"
+  		},
+  	},
+  },
 }
